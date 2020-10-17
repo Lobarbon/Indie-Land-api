e2cec8a94fa873607320a771ffff56e0ce3ac3da
Yuxuan <xuan19970825@gmail.com>
2020-10-18 02:42:36 +0800

    Test parser.to_data with "{}"

--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -7,23 +7,30 @@ require 'json'
 require 'http'
 # require_relative '../lib/apis/sport_api'
 # require_relative '../lib/apis/nusic_api'
+require_relative '../lib/parsers/basketball_parser.rb'
+# require_relative '../lib/parsers/indle_music_parser.rb'
 
 USERNAME = 'soumyaray'.freeze
 PROJECT_NAME = 'YPBT-app'.freeze
-CONFIG = YAML.load_file('./config/secrets.yaml')
+CONFIG = YAML.load_file('config/secrets.yaml')
 BASKETBALL_URL = CONFIG['sports_api']['basketball_url']
-# INDIE_MUSIC_URL = CONFIG['music_api']['indie_music_url']
-SPORT_YAML = YAML.load_file('./spec/fixtures/sports_result.yaml')
-# MUSIC_YAML = YAML::load_file('spec/fixtures/nusic_results.yml')
+MUSIC_URL = CONFIG['music_api']['indie_music_url']
 
 describe 'Tests API library' do
+  before do
+    `ruby lib/main.rb`
+    SPORT_YAML = YAML.load_file('spec/fixtures/sports_result.yaml')
+    MUSIC_YAML = YAML.load_file('spec/fixtures/music_results.yml')
+    @sport_json = JSON.parse(HTTP.get(BASKETBALL_URL))
+    @music_json = JSON.parse(HTTP.get(MUSIC_URL))
+  end
+
   describe 'Sport information' do
-    it 'should match the number of records' do
-      @sport_json = JSON.parse(HTTP.get(BASKETBALL_URL))
+    it 'Happy : should match the number of records' do
       _(SPORT_YAML.count).must_equal([11, @sport_json['result']['r']].min)
     end
 
-    it 'should match the first record' do
+    it 'Happy : should match the first record' do
       first_record = @sport_json['result']['d'][0]
       _(SPORT_YAML[0]['time']).must_equal first_record['kdt']
       _(SPORT_YAML[0]['country']).must_equal first_record['cn'][0]
@@ -31,9 +38,18 @@ describe 'Tests API library' do
       _(SPORT_YAML[0]['away_team']).must_equal first_record['atn'][0]
       _(SPORT_YAML[0]['host_team']).must_equal first_record['htn'][0]
     end
+
+    it 'SAD: parser should raise exception when given null json' do
+      parsed_data = BasketballJsonParser.new('{}').to_data
+      _(parsed_data).must_equal []
+    end
   end
 
   describe 'Music information' do
     # TODO
+  end
+
+  after do
+    `rm ./spec/fixtures/*.yaml > /dev/null 2>&1;`
   end
 end
