309c3f4a35c44bb85c71a111d67e0f29eb8159d6
Pin-Yu Wang <vito1218@gmail.com>
2020-10-23 21:59:00 +0800

    Install simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# NOTICE: Please ensure simplecov is required at the top of this file
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/rg'
 
