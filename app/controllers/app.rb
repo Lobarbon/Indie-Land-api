d5453e47982a0f0cacb7b98c5221d8254c4ddef6
daviswww <ho861206@gmail.com>
2020-10-31 16:19:33 +0800

    App route

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -8,11 +8,30 @@ require 'json'
 class App < Roda
   plugin :render, views: './app/views/', escape: true
   plugin :assets, path: './app/views/assets', css: ['style.css'], js: ['script.js']
-  route do |rt|
-    rt.assets
-    rt.get do
+  plugin :halt
+
+  route do |routing|
+    routing.assets
+    routing.root do
       @title = 'home'
       view 'home/index'
+    end
+    
+    routing.on 'room' do
+      routing.is do
+        routing.post do
+          @title = 'room'
+          first_name = routing.params['first_name'].capitalize
+          last_name = routing.params['last_name'].capitalize
+          @msg = 'Not fun ' + first_name + ' ' + last_name + 'hahahah'
+          view 'room/index'
+        end
+        routing.get do
+          @title = 'room'
+          @msg = 'Go back to post your name!'
+          view 'room/index'
+        end
+      end
     end
   end
 end
