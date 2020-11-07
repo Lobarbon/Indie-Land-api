6b04750cfc27c056b8685b559f3bee8480baa350
Pin-Yu Wang <vito1218@gmail.com>
2020-11-07 22:17:04 +0800

    Revise comments

--- app/infrastructure/database/orms/event_orm.rb
+++ app/infrastructure/database/orms/event_orm.rb
@@ -8,7 +8,7 @@ module IndieLand
     class EventOrm < Sequel::Model(:events)
       one_to_many :sessions,
                   # It would be better to pass class & key explicitly,
-                  # because we may change the class name & key name which are not in line with the convention.
+                  # because we may change the class name & key name, which are not in line with the convention.
                   class: :'IndieLand::Database::SessionOrm',
                   key: :id
 
--- app/infrastructure/database/orms/session_orm.rb
+++ app/infrastructure/database/orms/session_orm.rb
@@ -8,7 +8,7 @@ module IndieLand
     class SessionOrm < Sequel::Model(:sessions)
       many_to_one :event,
                   # It would be better to pass class & key explicitly,
-                  # because we may change the class name & key name which are not in line with the convention.
+                  # because we may change the class name & key name, which are not in line with the convention.
                   class: :'IndieLand::Database::EventOrm',
                   key: :id
 
