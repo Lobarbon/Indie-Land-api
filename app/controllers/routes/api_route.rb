91d4ba14ec652a8f9c6210ea733eda0418669ff7
Pin-Yu Wang <vito1218@gmail.com>
2020-11-12 11:05:35 +0800

    Fix reek issues

--- app/controllers/routes/api_route.rb
+++ app/controllers/routes/api_route.rb
@@ -5,19 +5,18 @@ require 'json'
 module IndieLand
   # Api routes
   class App
-    hash_branch 'api' do |r|
-      r.on 'music' do
+    hash_branch 'api' do |routing|
+      routing.on 'music' do
         response['Content-Type'] = 'application/json; charset=utf-8'
         @events = IndieLand::MusicEventsMapper.new.find_events
         IndieLand::Repository::For.entity(@events[0]).create_many(@events)
 
-        r.get Integer do |event_id|
-          # event_id = IndieLand::Repository::For.entity(@events[0]).find_all[1].event_id
-          a = IndieLand::Repository::Events.find_id(event_id)
-          a.nil? ? '' : a.to_attr_hash.to_json
+        routing.get Integer do |event_id|
+          event = IndieLand::Repository::Events.find_id(event_id)
+          event.nil? ? '' : event.to_attr_hash.to_json
         end
 
-        r.get do
+        routing.get do
           @events.map(&:to_attr_hash).to_json
         end
       end
