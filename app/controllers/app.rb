7ec6a2ae2ca0b9460fb3479c986e22c2527bb59e
daviswww <ho861206@gmail.com>
2020-11-14 18:54:11 +0800

    Add image

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -10,10 +10,16 @@ module IndieLand
   class App < Roda
     plugin :render, views: './app/views/', escape: true
     plugin :assets, path: './app/views/assets', css: ['style.css'], js: ['script.js']
+    plugin :public
+    # Use /path/to/app/static as location of files
+    opts[:root] = '/path/to/app'
+    plugin :public, root: 'static'
+
     plugin :hash_routes
     plugin :halt
 
     route do |routing|
+      routing.public
       routing.assets
       routing.hash_routes
       routing.root do
@@ -21,13 +27,16 @@ module IndieLand
 
         view 'home/index'
       end
-
+      routing.on(:static) do
+        routing.public
+      end
+      
       routing.on 'room' do
         routing.is do
           routing.get do
             @title = 'room'
             @events = IndieLand::Repository::For.klass(IndieLand::Entity::Event).find_all
-            view 'room/index'
+            view 'room/session'
           end
         end
       end
