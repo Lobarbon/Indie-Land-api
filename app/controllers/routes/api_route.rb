7c5edac4f66f5be4169fe84fbdd89100ca5d140f
daviswww <ho861206@gmail.com>
2020-11-15 10:15:52 +0800

    Create route

--- app/controllers/routes/api_route.rb
+++ app/controllers/routes/api_route.rb
@@ -1,16 +1,32 @@
 # frozen_string_literal: true
-
+require 'date'
 require 'json'
 
 module IndieLand
   # Api routes
   class App
     hash_branch 'api' do |routing|
-      routing.on 'music' do
+      routing.on 'events' do
         response['Content-Type'] = 'application/json; charset=utf-8'
         @events = IndieLand::MusicEventsMapper.new.find_events
         IndieLand::Repository::For.entity(@events[0]).create_many(@events)
 
+        routing.get Integer, Integer, Integer do |yy, mm, dd|
+          date = Date.new(yy, mm, dd)
+          future_events = IndieLand::Repository::Events.future_events
+          future_events.events_on_this_date(date).to_json
+        end
+
+        routing.get do
+          future_events = IndieLand::Repository::Events.future_events
+          future_events.future_dates.to_json
+        end
+
+
+      end
+
+      routing.on 'eventsId' do
+        response['Content-Type'] = 'application/json; charset=utf-8'
         routing.get Integer do |event_id|
           event = IndieLand::Repository::Events.find_id(event_id)
           event.nil? ? '' : event.to_attr_hash.to_json
