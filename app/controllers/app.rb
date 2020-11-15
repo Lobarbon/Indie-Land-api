202c2fbd68adb5dd9c8cc70e2b201e95f0912d49
daviswww <ho861206@gmail.com>
2020-11-15 10:16:00 +0800

    Add events

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -9,28 +9,23 @@ module IndieLand
   # Main routing rules
   class App < Roda
     plugin :render, views: './app/views/', escape: true
-    plugin :assets, path: './app/views/assets', css: ['style.css'], js: ['script.js']
-    plugin :public
-    # Use /path/to/app/static as location of files
-    opts[:root] = '/path/to/app'
-    plugin :public, root: 'static'
-
+    plugin :assets, path: './app/views/assets', css: ['main-style.css', 'navbar.css', 'circular.css'], js: ['circletype.min.js', 'scroll_event.js', 'scroll_circular.js']
     plugin :hash_routes
     plugin :halt
+    # Use public folder as location of files
+    plugin :public
 
     route do |routing|
       routing.public
       routing.assets
       routing.hash_routes
-      routing.root do
-        @title = 'home'
 
+      routing.root do
+        @future_events = IndieLand::Repository::Events.future_events
+        @future_dates = @future_events.future_dates
         view 'home/index'
       end
-      routing.on(:static) do
-        routing.public
-      end
-      
+
       routing.on 'room' do
         routing.is do
           routing.get do
