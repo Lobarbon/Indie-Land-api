dbb118f26e15beed7d0ec3d77eac03f8d54c0f3c
Pin-Yu Wang <vito1218@gmail.com>
2020-11-19 15:01:45 +0800

    Start with new views

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -14,21 +14,18 @@ module IndieLand
     plugin :hash_routes
 
     plugin :render, views: './app/views/', escape: true
-    plugin :assets, path: './app/views/assets',
-                    css: ['main-style.css', 'navbar.css', 'circular.css'],
-                    js: ['circletype.min.js', 'scroll_event.js', 'scroll_circular.js']
 
     route do |routing|
       routing.public
-      routing.assets
+      # routing.assets
       routing.hash_routes
 
       routing.root do
         session[:history] ||= []
 
         # get updated events information from the api
-        @events = IndieLand::MusicEventsMapper.new.find_events
-        IndieLand::Repository::For.entity(@events[0]).create_many(@events)
+        # @events = IndieLand::MusicEventsMapper.new.find_events
+        # IndieLand::Repository::For.entity(@events[0]).create_many(@events)
 
         @future_events = IndieLand::Repository::Events.future_events
         @future_dates = @future_events.future_dates
