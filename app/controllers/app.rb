416b688b71bef225e36fc04ca5a3fdd69613b1ff
Pin-Yu Wang <vito1218@gmail.com>
2020-11-20 14:37:46 +0800

    Start using logger

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -8,6 +8,8 @@ require 'json'
 module IndieLand
   # Main routing rules
   class App < Roda
+    logger = AppLogger.logger
+
     plugin :halt
     plugin :flash
     plugin :public # Use public folder as location of files
@@ -15,6 +17,7 @@ module IndieLand
 
     plugin :render, views: './app/views/', escape: true
 
+    # rubocop:disable Metrics/BlockLength
     route do |routing|
       routing.public
       # routing.assets
@@ -24,16 +27,29 @@ module IndieLand
         session[:history] ||= []
 
         # get updated events information from the api
-        @events = IndieLand::MusicEventsMapper.new.find_events
+        begin
+          logger.info('Getting events from api')
+          @events = IndieLand::MusicEventsMapper.new.find_events
+        # rubocop:disable Naming/RescuedExceptionsVariableName
+        rescue StandardError => error
+          logger.error(error.backtrace.join("\n"))
+          flash.now[:error] = 'Error occurs at fetching api'
+        end
+        # rubocop:enable Naming/RescuedExceptionsVariableName
+
+        logger.info('Writting events to database')
         IndieLand::Repository::For.entity(@events[0]).create_many(@events)
 
+        logger.info('Finding future events from database')
         @future_events = IndieLand::Repository::Events.future_events
         @future_dates = @future_events.future_dates
 
+        logger.info('Done!')
+
         if @future_dates.empty?
-          flash.now[:error] = 'Sorry! there are some problems in the server, please come back later.'
+          flash.now[:error] = 'Sorry! there are some problems from the server, please come back later.'
         else
-          flash.now[:notice] = 'Welcome to Indie-Land'
+          flash.now[:notice] = 'Welcome to Indie-Land.'
         end
 
         view 'home/index'
@@ -50,5 +66,6 @@ module IndieLand
         end
       end
     end
+    # rubocop:enable Metrics/BlockLength
   end
 end
