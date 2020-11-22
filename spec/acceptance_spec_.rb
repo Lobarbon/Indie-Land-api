d65cddd9c742f23338a464fe35ee08826ebddb0f
Yuxuan <xuan19970825@gmail.com>
2020-11-22 16:34:46 +0800

    Add github button check

--- spec/acceptance_spec_.rb
+++ spec/acceptance_spec_.rb
@@ -9,6 +9,7 @@ require 'watir'
 # rubocop:disable Metrics/BlockLength
 describe 'Acceptance Tests' do
   DatabaseHelper.setup_database_cleaner
+  GITHUB = 'https://github.com/Lobarbon/Indie-Land'
 
   before do
     DatabaseHelper.wipe_database
@@ -23,7 +24,6 @@ describe 'Acceptance Tests' do
 
   describe 'Homepage' do
     it 'HAPPY: should has a url on each event' do
-      # TODO
       # GIVEN: user is on the home page
       @browser.goto homepage
 
@@ -34,24 +34,38 @@ describe 'Acceptance Tests' do
       _(@browser.element(xpath: '//li/a').present?).must_equal true
     end
 
+    it 'HAAPY: should be able to click the github button' do
+      # GIVEN: user is on the home page
+      @browser.goto homepage
+
+      # WHEN: they click the github button
+      @browser.element(visible_text:'GitHub').click
+
+      # THEN: they should go to our github project's page
+      @browser.url.must_equal GITHUB
+    end
+
     it 'HAAPY: should be able to click the tag button' do
       # TODO
     end
+
     it 'HAPPY: should be able to like each event' do
       # TODO
     end
   end
 
   describe 'Event page' do
-    it 'HAPPY: should be able to see sessions of the events' do
-      # TODO
-    end
-    it 'HAPPY: should see number of likes' do
-      # TODO
-    end
-    it 'HAPPY: should be able to leave comments' do
-      # TODO
-    end
+    # it 'HAPPY: should be able to see sessions of the events' do
+    #   # TODO
+    # end
+
+    # it 'HAPPY: should see number of likes' do
+    #   # TODO
+    # end
+
+    # it 'HAPPY: should be able to leave comments' do
+    #   # TODO
+    # end
   end
 end
 # rubocop:enable Metrics/BlockLength
